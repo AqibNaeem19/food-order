fb0be398244dd8802a4d562becb2c1e5e8711a55
Aqib Naeem <maqibnaeem2019@gmail.com>
2021-09-10 11:26:43 +0500

    Add items to Cart and Update

--- src/Components/Meals/MealItem/MealItem.js
+++ src/Components/Meals/MealItem/MealItem.js
@@ -1,9 +1,21 @@
-import React from "react";
-import MealItemForm from './MealItemForm';
-import classes from './MealItem.module.css';
+import React, { useContext } from "react";
+import MealItemForm from "./MealItemForm";
+import CartContext from "../../../store/cart-context";
+import classes from "./MealItem.module.css";
 
 const MealItem = (props) => {
+  const cartCTX = useContext(CartContext);
   const price = `$${props.price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCTX.addItem({
+      id: props.id,
+      name: props.name,
+      amount: amount,
+      price: props.price,
+    });
+  };
+
   return (
     <li className={classes.meal}>
       <div>
@@ -12,7 +24,7 @@ const MealItem = (props) => {
         <div className={classes.price}>{price}</div>
       </div>
       <div>
-        <MealItemForm />
+        <MealItemForm onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
