6a2d9865e3895fe6bbf8c6ec3d810a7ff91adff7
Aqib Naeem <maqibnaeem2019@gmail.com>
2021-09-10 10:56:59 +0500

    Managing State of an app and Context

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,15 +1,24 @@
-import React from 'react';
-import CartIcon from '../Cart/CartIcon';
-import classes from './HeaderCartButton.module.css';
+import React, { useContext } from "react";
+import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
+import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  
+  // passing component to useContext
+  const cartCTX = useContext(CartContext);
+  // calculating total number of items in cart
+  const numberOfCartItems = cartCTX.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
